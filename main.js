7975cf4128c7bd5f4ada2f7bb50b40c2bc18cb2a
sheRaidan <carbin@sheridancollege.ca>
2026-09-14 17:08:17 -0400

    Refactor main.js for improved readability

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ function show(div){
     block.innerHTML = `<h2>${dataCollect[div].name}</h2><p>Created by: <b>${dataCollect[div].creator} in ${dataCollect[div].year}</b></p>
         <div class="bubble" style="border-color: hsl(${div*360/lengthOfFile.length}, 90%, 40%);"><p>${dataCollect[div].desc}</p>
         </div><div class="bubble" style="border-color: hsl(${div*360/lengthOfFile.length}, 90%, 40%);"><p>Hello -> ${dataCollect[div].sample}</p></div>`;
+    console.log(div*360/lengthOfFile.length +" was supposed to be "+div+"*"+360+"/"+lengthOfFile.length);
 
     for (var i = 0; i < lengthOfFile; i++){
         if (document.getElementById(i).style.top != "75vh" && i != div){
