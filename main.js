498d2c7279f76ce8524bf6cb231a957a76e625bb
sheRaidan <carbin@sheridancollege.ca>
2026-09-14 12:59:39 -0400

    Refactor language fetching and data handling
    Removed unused variable lengthOfFile and added dataCollect array to store language data.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var page = document.querySelector("body");
-var lengthOfFile = 0;
+// var lengthOfFile = 0;
 var places = [];
+var dataCollect = [];
 
 fetch("conlangs.json")
 .then(data => data.json())
@@ -12,22 +13,23 @@ fetch("conlangs.json")
         document.getElementById(num).style.width = 150/json.length+"vw";
         document.getElementById(num).style.background = `hsl(${num*360/json.length}, 80%, 80%)`;
         num++;
-        lengthOfFile++;
+        // lengthOfFile++;
         places.push(num*100/json.length);
+        dataCollect.push(lang);
     });
-    console.log(lengthOfFile);
-
-    // lastShown = json.length;
+    // console.log(lengthOfFile);
 })
 
 function show(div){
-    document.getElementById(div).style.top = "38vh";
-    document.getElementById(div).style.left = "38vw";
-    document.getElementById(div).classList.add("shown");
+    var block = document.getElementById(div);
+    block.style.top = "38vh";
+    block.style.left = "38vw";
+    block.classList.add("shown");
+    block.innerHTML = `<h2>${dataCollect[div].name}</h2>`;
 
     for (var i = 0; i < lengthOfFile; i++){
         if (document.getElementById(i).style.top != "75vh" && i != div){
-            document.getElementById(div).classList.remove("shown");
+            // document.getElementById(div).classList.remove("shown");
             document.getElementById(i).style.top = "75vh";
             document.getElementById(i).style.left = places[i]-places[0]/1000*999+"vw"; // needs the 1k/999 because places[i] on its own jumps over right for some reason
             // document.getElementById(i).style.transition = "all 0.2s";
