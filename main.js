0906a51b1552b7f605f87fad70f189e361768522
sheRaidan <carbin@sheridancollege.ca>
2026-09-14 13:26:58 -0400

    Refactor main.js to restore original functionality

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ function show(div){
 
     for (var i = 0; i < lengthOfFile; i++){
         if (document.getElementById(i).style.top != "75vh" && i != div){
-            document.getElementById(div).classList.remove("shown");
+            document.getElementById(i).classList.remove("shown");
             // document.getElementById(i).style.top = "75vh";
             // document.getElementById(i).style.left = places[i]-places[0]/1000*999+"vw"; // needs the 1k/999 because places[i] on its own jumps over right for some reason
             // document.getElementById(i).style.transition = "all 0.2s";
