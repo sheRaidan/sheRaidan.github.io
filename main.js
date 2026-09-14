ed50088f39be8b44c85d0bdb606d5a04cfaf424a
sheRaidan <carbin@sheridancollege.ca>
2026-09-14 13:06:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -22,16 +22,16 @@ fetch("conlangs.json")
 
 function show(div){
     var block = document.getElementById(div);
-    block.style.top = "38vh";
-    block.style.left = "38vw";
+    // block.style.top = "38vh";
+    // block.style.left = "38vw";
     block.classList.add("shown");
     block.innerHTML = `<h2>${dataCollect[div].name}</h2>`;
 
     for (var i = 0; i < lengthOfFile; i++){
         if (document.getElementById(i).style.top != "75vh" && i != div){
-            // document.getElementById(div).classList.remove("shown");
-            document.getElementById(i).style.top = "75vh";
-            document.getElementById(i).style.left = places[i]-places[0]/1000*999+"vw"; // needs the 1k/999 because places[i] on its own jumps over right for some reason
+            document.getElementById(div).classList.remove("shown");
+            // document.getElementById(i).style.top = "75vh";
+            // document.getElementById(i).style.left = places[i]-places[0]/1000*999+"vw"; // needs the 1k/999 because places[i] on its own jumps over right for some reason
             // document.getElementById(i).style.transition = "all 0.2s";
         }
     }
